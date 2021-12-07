f3e1e9c431dbf314961116853a66ebfb2190001d
Jheave <jimenezjheave123@gmail.com>
2021-12-08 03:14:29 +0800

    refractor

--- addUser.php
+++ addUser.php
@@ -214,4 +214,3 @@
   <?php include('components/footer-scripts.php'); ?>  
 </body>
 </html>
-<!-- px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700 -->
