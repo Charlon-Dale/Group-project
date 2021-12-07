ba69ecbc8186c699af1fbe2c7599390981de871b
Jheave <jimenezjheave123@gmail.com>
2021-12-08 03:03:01 +0800

    add darkmode style

--- addUser.php
+++ addUser.php
@@ -1,7 +1,19 @@
 <?php
     include('includes/functions.php');
     $listAllStudents = selectAllStudents();
+
+    if (isset($_POST['btnInsert'])) :
+      $Firstname = $_POST['firstname'];
+      $LastName = $_POST['lastname'];
+      $Birthday = $_POST['birthday'];
+      $Course = $_POST['course'];
+      $Email = $_POST['email'];
+      $Username = $_POST['username'];
+      $Password = $_POST['password'];
+      createUser($Firstname, $LastName, $Birthday, $Course, $Email, $Username, $Password);
+  endif;
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,130 +24,6 @@
     <?php include('components/header-scripts.php'); ?>
 </head>
 <body>
-<style>
-  /* Compiled dark classes from Tailwind */
-  .dark .dark\:divide-gray-700 > :not([hidden]) ~ :not([hidden]) {
-    border-color: rgba(55, 65, 81);
-  }
-  .dark .dark\:bg-gray-50 {
-    background-color: rgba(249, 250, 251);
-  }
-  .dark .dark\:bg-gray-100 {
-    background-color: rgba(243, 244, 246);
-  }
-  .dark .dark\:bg-gray-600 {
-    background-color: rgba(75, 85, 99);
-  }
-  .dark .dark\:bg-gray-700 {
-    background-color: rgba(55, 65, 81);
-  }
-  .dark .dark\:bg-gray-800 {
-    background-color: rgba(31, 41, 55);
-  }
-  .dark .dark\:bg-gray-900 {
-    background-color: rgba(17, 24, 39);
-  }
-  .dark .dark\:bg-red-700 {
-    background-color: rgba(185, 28, 28);
-  }
-  .dark .dark\:bg-green-700 {
-    background-color: rgba(4, 120, 87);
-  }
-  .dark .dark\:hover\:bg-gray-200:hover {
-    background-color: rgba(229, 231, 235);
-  }
-  .dark .dark\:hover\:bg-gray-600:hover {
-    background-color: rgba(75, 85, 99);
-  }
-  .dark .dark\:hover\:bg-gray-700:hover {
-    background-color: rgba(55, 65, 81);
-  }
-  .dark .dark\:hover\:bg-gray-900:hover {
-    background-color: rgba(17, 24, 39);
-  }
-  .dark .dark\:border-gray-100 {
-    border-color: rgba(243, 244, 246);
-  }
-  .dark .dark\:border-gray-400 {
-    border-color: rgba(156, 163, 175);
-  }
-  .dark .dark\:border-gray-500 {
-    border-color: rgba(107, 114, 128);
-  }
-  .dark .dark\:border-gray-600 {
-    border-color: rgba(75, 85, 99);
-  }
-  .dark .dark\:border-gray-700 {
-    border-color: rgba(55, 65, 81);
-  }
-  .dark .dark\:border-gray-900 {
-    border-color: rgba(17, 24, 39);
-  }
-  .dark .dark\:hover\:border-gray-800:hover {
-    border-color: rgba(31, 41, 55);
-  }
-  .dark .dark\:text-white {
-    color: rgba(255, 255, 255);
-  }
-  .dark .dark\:text-gray-50 {
-    color: rgba(249, 250, 251);
-  }
-  .dark .dark\:text-gray-100 {
-    color: rgba(243, 244, 246);
-  }
-  .dark .dark\:text-gray-200 {
-    color: rgba(229, 231, 235);
-  }
-  .dark .dark\:text-gray-400 {
-    color: rgba(156, 163, 175);
-  }
-  .dark .dark\:text-gray-500 {
-    color: rgba(107, 114, 128);
-  }
-  .dark .dark\:text-gray-700 {
-    color: rgba(55, 65, 81);
-  }
-  .dark .dark\:text-gray-800 {
-    color: rgba(31, 41, 55);
-  }
-  .dark .dark\:text-red-100 {
-    color: rgba(254, 226, 226);
-  }
-  .dark .dark\:text-green-100 {
-    color: rgba(209, 250, 229);
-  }
-  .dark .dark\:text-blue-400 {
-    color: rgba(96, 165, 250);
-  }
-  .dark .group:hover .dark\:group-hover\:text-gray-500 {
-    color: rgba(107, 114, 128);
-  }
-  .dark .group:focus .dark\:group-focus\:text-gray-700 {
-    color: rgba(55, 65, 81);
-  }
-  .dark .dark\:hover\:text-gray-100:hover {
-    color: rgba(243, 244, 246);
-  }
-  .dark .dark\:hover\:text-blue-500:hover {
-    color: rgba(59, 130, 246);
-  }
-  .dark .dark\:focus\:ring-gray-600:focus {
-    --tw-ring-color:  rgba(75, 85, 99);
-  }
-
-  /* Custom style */
-  .header-right {
-      width: calc(100% - 3.5rem);
-  }
-  .sidebar:hover {
-      width: 16rem;
-  }
-  @media only screen and (min-width: 768px) {
-      .header-right {
-          width: calc(100% - 16rem);
-      }        
-  }
-  </style>
 <div x-data="setup()" :class="{ 'dark': isDark }">
     <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-200 dark:bg-gray-700 text-black dark:text-white">
       <!-- Header -->
@@ -217,10 +105,9 @@
 			    	    			</label>
 			    	    			<input
 			    	    				class="w-full px-3 py-2 text-sm leading-tight text-gray-600 dark:text-gray-400 border rounded focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:ring-gray-600 focus:border-transparent"
-			    	    				id="firstName"
+			    	    				id="firstname"
                         name="firstname"
 			    	    				type="text"
-                        x-text="input"
 			    	    				placeholder="First Name"
 			    	    			/>
 			    	    		</div>
@@ -230,7 +117,7 @@
 			    	    			</label>
 			    	    			<input
 			    	    				class="w-full px-3 py-2 text-sm leading-tight text-gray-600 dark:text-gray-400 border rounded focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:ring-gray-600 focus:border-transparent"
-			    	    				id="lastName"
+			    	    				id="lastname"
 			    	    				name="lastname"
 			    	    				type="text"
 			    	    				placeholder="Last Name"
@@ -324,7 +211,7 @@
       </div>
     </div>
   </div>    
-
+  <?php include('components/footer-scripts.php'); ?>  
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
   <script>
     const setup = () => {
