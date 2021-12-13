3fd02e1e7b88c8fcccd8fca10a7f67ece323b325
Jheave <jimenezjheave123@gmail.com>
2021-12-13 19:03:38 +0800

    fix todo list ui

--- student-index.php
+++ student-index.php
@@ -28,7 +28,7 @@
               </div>
             </li>
             <li>
-              <a href="index.php" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+              <a  href="#" class="relative flex flex-row items-center h-11 focus:outline-none bg-blue-800 dark:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent border-blue-500 dark:border-gray-900 pr-6">
                 <span class="inline-flex justify-center items-center ml-4">
                   <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                 </span>
@@ -55,22 +55,22 @@
       <!-- ./Sidebar -->
     
       <div class="h-full ml-14 mt-14 mb-10 md:ml-64">
-        <div class='container mt-16 mx-auto lg:w-2/5 md:w-1/2 sm:w-7/8 bg-white shadow-lg rounded-lg'>
+        <div class='container mt-16 mx-auto lg:w-2/5 md:w-1/2 sm:w-7/8 bg-white dark:bg-gray-900 shadow-lg rounded-lg'>
           <div class='p-6'>
             <h1 class='text-2xl text-center'>Your Todo</h1>
-            <div class="border-t border-gray-100 my-2"></div>
+            <div class="border-t border-transparent border-blue-200 dark:border-gray-400 my-2"></div>
               <div x-data="state">  
                 <div class='mb-4'>
                     <div class="hidden sm:block">
-                        <div class="border-b border-gray-200">
+                        <div class="border-b border-transparent border-blue-200 dark:border-gray-400">
                             <nav class="-mb-px flex">
                                 <a href="#"
-                                    :class=" show_todo ? 'w-1/2 py-4 px-1 text-center border-b-2 border-indigo-500 font-medium text-sm leading-5 text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700' : 'w-1/2 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300'"
+                                    :class=" show_todo ? 'w-1/2 py-4 px-1 text-center border-b-4  border-blue-500 dark:border-gray-400 font-medium text-sm leading-5 text-gray-600 dark:text-gray-400 focus:outline-none  dark:focus:border-gray-900' : 'w-1/2 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300'"
                                     @click.prevent="show_todo = true">
                                     Todo
                                 </a>
                                 <a href="#"
-                                    :class=" !show_todo ? 'w-1/2 py-4 px-1 text-center border-b-2 border-indigo-500 font-medium text-sm leading-5 text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700' : 'w-1/2 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300'"
+                                    :class=" !show_todo ? 'w-1/2 py-4 px-1 text-center border-b-4 border-blue-500 dark:border-gray-400 font-medium text-sm leading-5 text-gray-600 dark:text-gray-400 focus:outline-none dark:focus:border-gray-900' : 'w-1/2 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300'"
                                     @click.prevent="show_todo = false">
                                     Done
                                 </a>
@@ -84,9 +84,9 @@
                     <div x-show="todo.todo === show_todo" class='transform transition origin-top'>
                         <div class=" flex items-center">
                             <input :id=" todo.id" type="checkbox"
-                                class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                                class="form-checkbox h-4 w-4 text-gray-600 dark:text-gray-400 transition duration-150 ease-in-out"
                                 :checked="!todo.todo" @click="setTimeout(function() {todo.todo = !todo.todo}, 1000)" />
-                            <label :for="todo.id" class="ml-2 block text-md leading-6 text-gray-900">
+                            <label :for="todo.id" class="ml-2 block text-sm font-bold leading-6 text-gray-600 dark:text-gray-400">
                                 <span x-text="todo.text"></span>
                             </label>
                         </div>
@@ -95,9 +95,12 @@
                 </template>
 
                 <div x-show="show_todo" class='mt-4'>
-                    <label for="add_todo" class="ml-2 block text-sm leading-5 text-gray-900">Add a todo:</label>
-                    <input type="text" id="add_todo" class='form-input'
-                        @keyup.enter="todos.push({id: (+ new Date()), text: $event.target.value, todo: true});">
+                    <label class="block mb-2 text-sm font-bold text-gray-600 dark:text-gray-400" for="add_todo">Add a todo:</label>
+                  
+                    <input type="text" id="add_todo" class='form-input w-full px-3 py-2 text-sm leading-tight text-gray-600 dark:text-gray-400 border rounded focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:ring-gray-600 focus:border-transparent'
+                      @keyup.enter="todos.push({id: (+ new Date()), text: $event.target.value, todo: true});"
+                      placeholder="Add a todo"
+                    >
                 </div>
               </div>
             </div>
